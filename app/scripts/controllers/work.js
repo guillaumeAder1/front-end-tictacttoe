de5e9e12cc24d1e875769a7ec66025fbd878bd80
guillaumeAder1 <GAder@esriuk.com>
2017-06-02 12:06:33 +0100

    add animation on list and add class on selected filter

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -9,22 +9,38 @@
  */
 angular.module('frontendApp')
     .controller('WorkCtrl', function($scope, $http) {
-
+        // init vars
         $scope.works = [];
         $scope.search = "all";
-        $scope.types = ["all", "web", "graphic"]
-        this.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+        // $scope.types = ["all", "web", "graphic"];
+        $scope.types = ["all"];
+        // get json data
+        $scope.getData = function() {
+            $http.get('works-data.json')
+                .then(function(res) {
+                    console.log(res);
+                    if (res.status !== 200) {
+                        return false;
+                    }
+                    $scope.works = res.data;
+                    for (var i in $scope.works) {
+                        if ($scope.types.indexOf($scope.works[i].type) === -1) {
+                            $scope.types.push($scope.works[i].type);
+                        }
+                    }
+                    console.log($scope.types);
+                });
+        };
+        // change filter (web / graphic)
+        $scope.selectFilter = function(val) {
+            console.log($scope.types[val]);
+            $scope.search = $scope.types[val];
 
-        $http.get('works-data.json')
-            .then(function(res) {
-                console.log(res);
-                if (res.status != 200) {
-                    return false;
-                }
-                $scope.works = res.data;
-            });
+        };
+
+        $scope.isSelected = function(index) {
+            return ($scope.search == $scope.types[index]) ? true : false;
+        };
+
+        $scope.getData();
     });
